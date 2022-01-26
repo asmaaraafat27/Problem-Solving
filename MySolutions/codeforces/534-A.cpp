9431e3b83ea9192c8db8a2c33e9270c32e701abd
Asmaa <89736386+asmaaraafat27@users.noreply.github.com>
2022-01-27 01:45:34 +0200

    Add files via upload

---
+++ MySolutions/codeforces/534-A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    ////"THINK TWICE... CODE ONCE"//.
+    fast;
+    int cnt =0;
+    int n;
+    cin>>n;
+    cout<<n<<endl;
+    for(int i=1; i<=n; i++)
+    {
+        cout<<1<<" ";
+    }
+}
