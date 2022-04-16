059552bd9eed10cd3fd594d3a6e067aaf2486f3a
Asmaa <asbeauty80@gmail.com>
2022-04-16 02:26:29 +0200

    Add files via upload

---
+++ MySolutions/codeforces/A-CF-469(Div. 2).cpp
@@ -0,0 +1,36 @@
+/*
+   Author: Asmaa Raafat.
+*/
+/////////////////////////////"THINK TWICE... CODE ONCE"/////////////////////////////////.
+#include <bits/stdc++.h>
+#define ll long long
+#define fast ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    //freopen("fight.txt", "rt", stdin);
+    #endif
+    fast;
+    int a, b, c;
+    cin>>a>>b>>c;
+    while(c--)
+    {
+        if(b > a)
+        {
+            a++;
+        }
+        else
+        {
+            b++;
+        }
+    }
+    cout<<min(a,b)*2;
+}
+/* stuff you should look for
+	* int overflow, array bounds
+	* special cases (n=1?)
+	* do smth instead of nothing and stay organized
+	* WRITE STUFF DOWN
+	* DON'T GET STUCK ON ONE APPROACH
+*/
